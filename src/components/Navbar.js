a9cf622e0428df4617c9e1d97cd9736c88779b29
Clare Hsu <clare.hsu390@gmail.com>
2018-02-08 22:40:26 -0800

    Add navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 export default class Navbar extends React.Component {
     
     render() {
         return(
             <div className="navbar">
-            <navigation>
-                {/* <a href="./projects">Projects</a> */}
-            </navigation>
+            <nav className="navbar__links">
+                <Link to="/about">About</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/contact">Contact</Link>
+
+            </nav>
             </div>
         );
     }
