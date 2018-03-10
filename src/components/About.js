d10eed24c8dc9c0d2f0a67abb32cd90ab378a7e4
Clare Hsu <clare.hsu390@gmail.com>
2018-03-09 16:18:24 -0800

    add projects

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ export default class About extends React.Component {
                 <img id="about-image" src={require("../IMG_3054.JPG")}/>
                 </span>
                 <article className="about__description">
-                    Clare is a software developer with a linguistics degree. She discovered her passion for coding when she realized it was like learning another language. Being able to communicate with technology creates endless possibilities. Wanting to pursue a technical career, she has now completed projects and continues to develop her technical repoitoire. Her passion currently lies in front-end development,creating visually appealing interfaces.  
+                    Clare is a software developer with a linguistics degree. She discovered her passion for coding when she realized it was like learning another language. Being able to communicate with technology creates endless possibilities. Wanting to pursue a technical career, she has now built web applications and continues to develop her technical repoitoire. Her passion currently lies in full stack development, creating visually appealing interfaces and working out complex problems on the server-side.  
                 </article>
                 </div>
             </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import './Projects.css';
 export default class Projects extends React.Component {
+    handleHover(e) {
+        e.target.classList.add('show');
+    }
     render() {
         return (
-            <div>
+            <div className="projects">
                 <h2>Projects</h2>
+                <section className="project-section">
+                    <div className="project" onMouseOver={() => this.handleHover.bind(this)}>
+                        <h3>Your Aesthetic</h3>
+                        <img className="project-images" src={require("../full-stack.png")}/>
+                        <div className="overlay">
+                            </div>
+                    </div>
+
+                <div className="project">
+                    <h3>Word Frenzy</h3>
+                    <img className="project-images" src={require("../frenzy.png")}/>
+                </div>
+
+                <div className="project">
+                    <h3>Clustering Visualization</h3>
+                    <img className="project-images" src={require("../flex.png")}/>
+                    </div>
+                    </section>
             </div>
         );
     }
